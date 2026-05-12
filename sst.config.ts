561dc55b439bc04d171cf015380aabc796684c65
aman brar <amanbrarpro@gmail.com>
2026-05-12 14:13:18 -0700

    feat: safe environment variable validation using @t3-oss/env-nextjs and inject configuration via SST

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
+import pkg from "./package.json";
 
 export default $config({
   app(input) {
@@ -11,9 +12,27 @@ export default $config({
     };
   },
   async run() {
+    const domain = "amanbrar.pro";
+    const isProduction = $app.stage === "production";
+
+    let appUrl: string;
+    if ($dev) {
+      appUrl = `http://localhost:3000`;
+    } else {
+      appUrl = isProduction
+        ? `https://${domain}`
+        : `https://${$app.stage}.${domain}`;
+    }
+
     new sst.aws.Nextjs("MyWeb", {
       dev: {
         command: "next dev",
+      },
+      environment: {
+        LOG_LEVEL: isProduction ? "info" : "debug",
+        APP_ENV: $dev ? "development" : $app.stage,
+        VERSION: pkg.version,
+        APP_URL: appUrl,
       },
     });
   },
