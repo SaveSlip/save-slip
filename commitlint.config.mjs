151268a82258a0f5dbfdf0e7902a0b9bb1e915dd
amanbrarpro <amanbrarpro@gmail.com>
2026-05-13 12:55:09 -0700

    feat: add production deployment (#11)

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,9 @@
-const config = { extends: ["@commitlint/config-conventional"] };
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [2, "always", 150],
+    "header-max-length": [2, "always", 150],
+  },
+};
 
 export default config;
